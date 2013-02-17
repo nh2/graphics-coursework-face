e706bd27aeb46b83f7ac5c659f6a40b68704e8de
Niklas Hambüchen <mail@nh2.me>
2013-02-17 15:57:14 +0000

    Cleanup

--- Coursework.hs
+++ Coursework.hs
@@ -21,34 +21,35 @@ data VTK = VTK
   } deriving (Eq, Show)
 
 
-vertexParser :: Parser Vertex
-vertexParser = Vertex <$> d <*> d <*> d
-  where
-    d = signed double <* skipSpace
-
-polygonParser :: Parser Polygon
-polygonParser = do
-  size <- decimal
-  Polygon . fromList <$> A.count size (skipSpace *> decimal)
-
-textureCoordParser :: Parser TextureCoordinate
-textureCoordParser = TextureCoordinate <$> (skipSpace *> double) <*> (skipSpace *> double)
-
+-- | Parses the contents of a .vtk file into a VTK data structure.
 vtkParser :: Parser VTK
 vtkParser = do
+  -- Parse header
   _ <- "# vtk DataFile Version 3.0" *> endl
-  desc <- endl
-  nPoints <- ("ASCII" *> endl *> "DATASET POLYDATA" *> endl *> "POINTS ") *> decimal <*. " float" <* endl
+  desc <- endl <* ("ASCII" *> endl *> "DATASET POLYDATA" *> endl)
+  -- Parse points
+  nPoints <- "POINTS " .*> decimal <*. " float" <* endl
   verts <- A.count nPoints (vertexParser <* skipSpace)
+  -- Parse polygons
   nPolys <- "POLYGONS " .*> decimal <* endl
   polys <- A.count nPolys (polygonParser <* endl)
+  -- Parse texture coordinates
   nTextures <- "POINT_DATA " *> decimal <* endl
-  _ <- endl
+  _ <- endl -- TEXTURE COORDINATES line
   when (nTextures /= nPoints) $ error "number of textures and points mismatch"
   texts <- A.count nTextures textureCoordParser
+  -- Done
   return $ VTK desc (fromList verts) (fromList polys) (fromList texts)
   where
     endl = takeTill isEndOfLine <* endOfLine
+    sd = signed double <* skipSpace
+
+    -- How to parse single Vertices, Polygons, texture coordinates
+    vertexParser = Vertex <$> sd <*> sd <*> sd
+    polygonParser = do
+      size <- decimal
+      Polygon . fromList <$> A.count size (skipSpace *> decimal)
+    textureCoordParser = TextureCoordinate <$> (skipSpace *> double) <*> (skipSpace *> double)
 
 
 main :: IO ()
