d4187c48d523559c7e0153d91c8dd35967630340
Niklas Hambüchen <mail@nh2.me>
2013-02-18 22:40:25 +0000

    Translate given cgRender.cpp to Haskell
    Only commented-in code so far.

--- Coursework.hs
+++ Coursework.hs
@@ -1,10 +1,66 @@
 import           Control.Monad
 import           Control.Applicative
-import           Data.Attoparsec.Text as A
 import qualified Data.Text.IO as Text
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           System.Environment
+import           System.Exit
 
 import           Parsing
 
+
+initGL :: IO ()
+initGL = do
+  clearColor $= Color4 0 0 0 0
+  putStrLn "init"
+
+
+display :: DisplayCallback
+display = do
+  clear [ColorBuffer, DepthBuffer]
+  putStrLn "display"
+
+
+reshape :: ReshapeCallback
+reshape size = do
+  putStrLn "reshape"
+
+  viewport $= (Position 0 0, size)
+
+
+keyboard :: KeyboardCallback
+keyboard key (Position _x _y) = case key of
+  '\ESC' -> exitSuccess
+  _      -> return ()
+
+
+initGraphics :: String -> [String] -> IO ()
+initGraphics progName args = do
+  initialDisplayCapabilities $= [ With DisplaySingle,
+                                  With DisplayRGB,
+                                  With DisplayDepth ]
+
+  initialWindowSize $= Size 256 256
+  initialWindowPosition $= Position 0 0
+
+  _ <- initialize progName args
+
+  _ <- createWindow progName
+
+  initGL
+
+  displayCallback $= display
+  reshapeCallback $= Just reshape
+  keyboardCallback $= Just keyboard
+
+  mainLoop
+
+
 main :: IO ()
 main = do
-  Text.getContents >>= (print . parse vtkParser)
+  -- Text.getContents >>= (print . parse vtkParser)
+
+  args <- getArgs
+  progName <- getProgName
+
+  initGraphics progName args
