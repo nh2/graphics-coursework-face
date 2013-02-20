36d319d38063faec094d65c9d88c40855b741139
Niklas Hambüchen <mail@nh2.me>
2013-02-20 03:50:13 +0000

    Improve key + mouse handling

--- Coursework.hs
+++ Coursework.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
+-- Yeah nice. If we give this module a name,
+-- rendering is 10x slower due to non-inlining of
+-- automatically exported functions like `changeVector`.
+-- Empty export list and INLINE pragmas help.
+-- module Main where
+
 import           Control.Applicative
-import           Control.Monad
 import           Control.Monad.Primitive
 import           Data.Attoparsec.Text as A
 import           Data.IORef
@@ -19,7 +24,8 @@ import           System.Exit
 import           Parsing as Input
 
 
-deg :: (Num a, Floating a) => a
+{-# INLINE deg #-}
+deg :: (Floating a) => a
 deg = 2 * pi / 180
 
 
@@ -48,6 +54,7 @@ initGL = do
   -- depthFunc $= Just Lequal
 
 
+{-# INLINE changeVector #-}
 changeVector :: (VUM.Unbox a, PrimMonad m) => VUM.MVector (PrimState m) a -> Int -> (a -> a) -> m ()
 changeVector v i f = VUM.read v i >>= (VUM.write v i . f)
 
@@ -78,22 +85,42 @@ reshape state size = do
   transform state
 
 
+redraw :: State -> IO ()
+redraw state = transform state >> postRedisplay Nothing
+
+
 keyboardMouse :: State -> KeyboardMouseCallback
-keyboardMouse state@State { horizAngleVar, vertAngleVar, distanceVar } key keyState _mods (Position _x _y) = do
-  when (keyState == Down) $ case key of
-    SpecialKey KeyLeft  -> horizAngleVar $~! (+    2 *deg ) >> redraw
-    SpecialKey KeyRight -> horizAngleVar $~! (+ (- 2 *deg)) >> redraw
-    SpecialKey KeyUp    -> vertAngleVar  $~! (+    2 *deg ) >> redraw
-    SpecialKey KeyDown  -> vertAngleVar  $~! (+ (- 2 *deg)) >> redraw
-
-    MouseButton WheelUp   -> distanceVar $~! (+ (- 5)) >> redraw
-    MouseButton WheelDown -> distanceVar $~! (+    5 ) >> redraw
-
-    Char '\ESC' -> exitSuccess
-
-    k -> putStrLn $ "unhandled key pressed: " ++ show k
+keyboardMouse state@State { horizAngleVar, vertAngleVar, distanceVar, lastMouseDragPosVar } = on
   where
-    redraw = transform state >> postRedisplay Nothing
+    on (SpecialKey KeyLeft)    Down _ _ = horizAngleVar $~! (+    2 *deg ) >> redraw state
+    on (SpecialKey KeyRight)   Down _ _ = horizAngleVar $~! (+ (- 2 *deg)) >> redraw state
+    on (SpecialKey KeyUp)      Down _ _ = vertAngleVar  $~! (+    2 *deg ) >> redraw state
+    on (SpecialKey KeyDown)    Down _ _ = vertAngleVar  $~! (+ (- 2 *deg)) >> redraw state
+
+    on (MouseButton WheelUp)   Down _ _ = distanceVar $~! (+ (- 5)) >> redraw state
+    on (MouseButton WheelDown) Down _ _ = distanceVar $~! (+    5 ) >> redraw state
+    on (MouseButton _)         Up   _ _ = lastMouseDragPosVar $= Nothing -- Reset drag on mouse up
+
+    on (Char '\ESC')           Down _ _ = exitSuccess
+
+    on k                       _    _ _ = putStrLn $ "unhandled key pressed: " ++ show k
+
+
+mouseMotion :: State -> MotionCallback
+mouseMotion state@State { lastMouseDragPosVar, horizAngleVar, vertAngleVar } pos@(Position x y) = do
+  lastDragPos <- get lastMouseDragPosVar
+
+  -- Always calculate drag from *drag start position*
+  case lastDragPos of
+    Nothing               -> lastMouseDragPosVar $= Just pos
+    Just (Position lx ly) -> do
+
+      horizAngleVar $~! (+ fromIntegral (-(x - lx)) / 2 *deg)
+      vertAngleVar  $~! (+ fromIntegral (  y - ly ) / 2 *deg)
+
+      lastMouseDragPosVar $= Just pos
+
+      redraw state
 
 
 transform :: State -> IO ()
@@ -154,13 +181,18 @@ initGraphics progName args vtk = do
       f                = realToFrac
       center           = Vertex3 (f a) (f b) (f c)
 
-  -- Initialize global variables
-  state <- State <$> newIORef (30 *deg) <*> newIORef 0 <*> newIORef 20 <*> newIORef center
+  -- Initialize global state
+  state <- State <$> newIORef (30 *deg) -- on-ground rotation angle
+                 <*> newIORef 0         -- into-the-air angle
+                 <*> newIORef 20        -- the distance to the center
+                 <*> newIORef center    -- the point around which we rotate ("center")
+                 <*> newIORef Nothing   -- last mouse position
 
   -- Initialize callback functions
   displayCallback $= display vtk vertexNormals
   reshapeCallback $= Just (reshape state)
   keyboardMouseCallback $= Just (keyboardMouse state)
+  motionCallback        $= Just (mouseMotion   state)
 
   -- Start renderingdisplay
   mainLoop
@@ -173,6 +205,7 @@ data State = State
   , vertAngleVar :: IORef Double
   , distanceVar :: IORef Double
   , centerVar :: IORef (Vertex3 GLdouble)
+  , lastMouseDragPosVar :: IORef (Maybe Position)
   }
 
 
